fd576e0028c94bd59d06636404b4728e7f291f01
miaa852012 <miaa852012@gmail.com>
2025-07-23 03:09:45 -0400

    Update script.js

--- photostest/script.js
+++ photostest/script.js
@@ -1,1 +1,26 @@
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 
+let index = 0;
+
+function updateSlide() {
+  slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+function nextSlide() {
+  index = (index + 1) % images.length;
+  updateSlide();
+}
+
+function prevSlide() {
+  index = (index - 1 + images.length) % images.length;
+  updateSlide();
+}
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+// Optional: auto slide every 4 seconds
+setInterval(nextSlide, 4000);
